ceeaede5549ab4a7742f7430567dbbf9479f3fea
tnvrravish <vt7@njit.edu>
2022-04-17 11:59:15 -0400

    edited app

--- app/__init__.py
+++ app/__init__.py
@@ -69,6 +69,13 @@ def create_app():
     # get root directory of project
     root = os.path.dirname(os.path.abspath(__file__))
     # set the name of the apps log folder to logs
+    logdir = os.path.join(root, 'logs')
+    # make a directory if it doesn't exist
+    if not os.path.exists(logdir):
+        os.mkdir(logdir)
+    # set name of the log file
+    log_file = os.path.join(logdir, 'info.log')
+
 
     return app
 
