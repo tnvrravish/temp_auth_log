ec72176f8d3ae44ad5d34521fef92cf9593c6907
tnvrravish <vt7@njit.edu>
2022-04-17 09:05:12 -0400

    app file edited

--- app/__init__.py
+++ app/__init__.py
@@ -30,18 +30,6 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 
-class RequestFormatter(logging.Formatter):
-    def format(self, record):
-        if has_request_context():
-            record.url = request.url
-            record.remote_addr = request.remote_addr
-        else:
-            record.url = None
-            record.remote_addr = None
-
-        return super().format(record)
-
-
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
@@ -67,75 +55,6 @@ def create_app():
     # Deactivate the default flask logger so that log messages don't get duplicated
     app.logger.removeHandler(default_handler)
 
-    # get root directory of project
-    root = os.path.dirname(os.path.abspath(__file__))
-    # set the name of the apps log folder to logs
-    logdir = os.path.join(root, 'logs')
-    # make a directory if it doesn't exist
-    if not os.path.exists(logdir):
-        os.mkdir(logdir)
-    # set name of the log file
-    log_file = os.path.join(logdir, 'info.log')
-
-    handler = logging.FileHandler(log_file)
-    # Create a log file formatter object to create the entry in the log
-    formatter = RequestFormatter(
-        '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
-        '%(levelname)s in %(module)s: %(message)s'
-    )
-    # set the formatter for the log entry
-    handler.setFormatter(formatter)
-    # Set the logging level of the file handler object so that it logs INFO and up
-    handler.setLevel(logging.INFO)
-    # Add the handler for the log entry
-    app.logger.addHandler(handler)
-
-    @app.before_request
-    def start_timer():
-        g.start = time.time()
-
-    @app.after_request
-    def log_request(response):
-        if request.path == '/favicon.ico':
-            return response
-        elif request.path.startswith('/static'):
-            return response
-        elif request.path.startswith('/bootstrap'):
-            return response
-
-        now = time.time()
-        duration = round(now - g.start, 2)
-        dt = datetime.datetime.fromtimestamp(now)
-        timestamp = rfc3339(dt, utc=True)
-
-        ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-        host = request.host.split(':', 1)[0]
-        args = dict(request.args)
-
-        log_params = [
-            ('method', request.method),
-            ('path', request.path),
-            ('status', response.status_code),
-            ('duration', duration),
-            ('time', timestamp),
-            ('ip', ip),
-            ('host', host),
-            ('params', args)
-        ]
-
-        request_id = request.headers.get('X-Request-ID')
-        if request_id:
-            log_params.append(('request_id', request_id))
-
-        parts = []
-        for name, value in log_params:
-            part = name + ': ' + str(value) + ', '
-            parts.append(part)
-        line = " ".join(parts)
-        #this triggers a log entry to be created with whatever is in the line variable
-        app.logger.info('this is the plain message')
-
-        return response
 
     return app
 
