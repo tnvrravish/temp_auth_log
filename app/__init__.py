fce580c99ae9c6da00c2f78a0435345428373e92
tnvrravish <vt7@njit.edu>
2022-04-17 09:57:31 -0400

    edited app

--- app/__init__.py
+++ app/__init__.py
@@ -104,38 +104,6 @@ def create_app():
         elif request.path.startswith('/bootstrap'):
             return response
 
-        now = time.time()
-        duration = round(now - g.start, 2)
-        dt = datetime.datetime.fromtimestamp(now)
-        timestamp = rfc3339(dt, utc=True)
-
-        ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-        host = request.host.split(':', 1)[0]
-        args = dict(request.args)
-
-        log_params = [
-            ('method', request.method),
-            ('path', request.path),
-            ('status', response.status_code),
-            ('duration', duration),
-            ('time', timestamp),
-            ('ip', ip),
-            ('host', host),
-            ('params', args)
-        ]
-
-        request_id = request.headers.get('X-Request-ID')
-        if request_id:
-            log_params.append(('request_id', request_id))
-
-        parts = []
-        for name, value in log_params:
-            part = name + ': ' + str(value) + ', '
-            parts.append(part)
-        line = " ".join(parts)
-        #this triggers a log entry to be created with whatever is in the line variable
-        app.logger.info('this is the plain message')
-
         return response
 
     return app
