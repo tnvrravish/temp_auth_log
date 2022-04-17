67e45677f739953a47426f583c7c19d222b503c2
tnvrravish <vt7@njit.edu>
2022-04-17 09:18:35 -0400

    file creation unit test added

--- tests/file_creation_test.py
+++ tests/file_creation_test.py
@@ -15,13 +15,13 @@ def test_database_file():
     root = os.path.dirname(os.path.abspath(__file__))
     database = os.path.join(root, '../database/db.sqlite')
     assert os.path.exists(database) == True
-"""
+
 def test_info_log_file():
     root = os.path.dirname(os.path.abspath(__file__))
     database = os.path.join(root, '../app/logs/mydebug.log')
     assert os.path.exists(database) == True
+
 def test_debug_log_file():
     root = os.path.dirname(os.path.abspath(__file__))
     database = os.path.join(root, '../app/logs/myinfo.log')
     assert os.path.exists(database) == True
-"""
